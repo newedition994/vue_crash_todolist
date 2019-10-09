5e09e26f304d5165d9ab0089feef4c0b3e967fbd
Breyanna Vass <breyannavass@gmail.com>
2019-10-08 21:35:55 -0400

    adding vue router and change

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,12 @@
 <template>
   <div id="app">
-    <Header />
-    <AddTodo v-on:add-todo="addTodo" />
-    <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo" />
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
   </div>
 </template>
-
-<script>
-import Todos from "./components/Todos";
-import Header from "./components/layout/Header";
-import AddTodo from "./components/AddTodo";
-import axios from "axios";
-
-export default {
-  name: "app",
-  components: {
-    Todos,
-    Header,
-    AddTodo
-  },
-  data() {
-    return {
-      todos: []
-    };
-  },
-  methods: {
-    deleteTodo(id) {
-      this.todos = this.todos.filter(todo => todo.id !== id);
-    },
-    addTodo(newTodo) {
-      this.todos = [...this.todos, newTodo];
-    }
-  },
-  created() {
-    axios
-      .get("https://jsonplaceholder.typicode.com/todos?_limit=5")
-      .then(res => (this.todos = res.data))
-      .catch(err => console.log(err));
-  }
-};
-</script>
 
 <style>
 * {
